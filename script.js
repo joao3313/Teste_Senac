ae5e0b432da513a059310f47fd7d7d30d913accc
joao3313 <ikkijp2012@gmail.com>
2024-09-11 21:26:02 -0300

    feat:HorarioSenac

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarHorarioSaida() {
+    const horarioSaida = "22:15";
+    alert("O horário de saída do curso de técnico de informática é às " + horarioSaida);
+}
+
+// Chama a função para exibir o horário
+mostrarHorarioSaida();
